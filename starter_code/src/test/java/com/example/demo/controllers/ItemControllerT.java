47d7bd08ac8f9c2f5a8d398797cd8f6802706bf8
kevin <onofrekevin@gmail.com>
2022-07-09 20:37:57 -0500

    Se agrego el metodo obtener items por su nombre.

--- starter_code/src/test/java/com/example/demo/controllers/ItemControllerT.java
+++ starter_code/src/test/java/com/example/demo/controllers/ItemControllerT.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.swing.*;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -30,4 +29,13 @@ public class ItemControllerT {
         Assert.assertEquals(2,item.getBody().size());
     }
 
+    //insert into item (name, price, description) values ('Round Widget', 2.99, 'A widget that is round');
+    @Test
+    public void getItemsByName(){
+        ResponseEntity<List<Item> > item = itemController.getItemsByName("Round Widget");
+        Assert.assertNotNull(item);
+        Assert.assertEquals(HttpStatus.OK, item.getStatusCode());
+        Item itemRetreive = item.getBody().get(0);
+        Assert.assertEquals("Round Widget",itemRetreive.getName());
+    }
 }
